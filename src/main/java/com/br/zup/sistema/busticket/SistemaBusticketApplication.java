5d0361b6e204b1ed6b04afb9d2d49995ae34ee79
Carol <carolina.oliveira@zup.com.br>
2019-07-08 17:45:26 -0300

    Iniciar projeto

---
+++ src/main/java/com/br/zup/sistema/busticket/SistemaBusticketApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.sistema.busticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBusticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBusticketApplication.class, args);
+	}
+
+}
