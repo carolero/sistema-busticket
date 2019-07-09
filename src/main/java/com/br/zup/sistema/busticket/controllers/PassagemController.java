060beb3d51c7bea96d56b4bb15c003290fffc298
Carol <carolina.oliveira@zup.com.br>
2019-07-08 21:43:13 -0300

    Validar compra e verificar idade

--- src/main/java/com/br/zup/sistema/busticket/controllers/PassagemController.java
+++ src/main/java/com/br/zup/sistema/busticket/controllers/PassagemController.java
@@ -3,8 +3,11 @@ package com.br.zup.sistema.busticket.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.br.zup.sistema.busticket.Models.ClienteModel;
 import com.br.zup.sistema.busticket.services.PassagemService;
 
 @Controller
@@ -19,6 +22,21 @@ public class PassagemController {
 		return modelAndView;
 	}
 	
+	@GetMapping("/cadastrar/{onibusId}")
+	public ModelAndView cadastrarCliente() {
+		ModelAndView modelAndView = new ModelAndView("cadastrar-clientes.html");
+		return modelAndView;
+	}
+	
+	@PostMapping("/cadastrar/{onibusId}")
+	public String finalizarCompra(@PathVariable int onibusId, ClienteModel clienteModel) {
+		if(clienteModel.getIdade() >= 18) {
+			passagemService.cadastrarCliente(onibusId, clienteModel);
+			return "redirect:/";
+		} else {
+			return "barrado.html";
+		}
+	}
 	
 	
 
